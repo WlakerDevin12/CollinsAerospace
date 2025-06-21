f917cc7a7b4f3a2e0c85699a1df98bfba4f0d141
WlakerDevin12 <walkerdevin176@gmail.com>
2025-06-21 15:28:24 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
-html_favicon = '_static/download.png'
+html_favicon = '_static/favicon.png'
 html_js_files = [
     'chatbot.js',
 ]
