295770f8be2ae97edb0ed6286d9c3bef6f381da9
WlakerDevin12 <walkerdevin176@gmail.com>
2025-06-21 15:27:16 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+project = 'CollinsAerospace'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+html_favicon = '_static/download.png'
+html_js_files = [
+    'chatbot.js',
+]
