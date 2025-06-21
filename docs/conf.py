1b78afc87a07f136109b4f792bb47a585cdc49f1
WlakerDevin12 <walkerdevin176@gmail.com>
2025-06-21 15:43:32 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -7,6 +7,9 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'alabaster'
+html_context = {
+    'google_site_verification': 'HojYGrilvLTCMn1nlF4jRxkr25mv_YKARAIvBy40OVc'
+}
 html_static_path = ['_static']
 html_favicon = '_static/favicon.png'
 html_js_files = [
